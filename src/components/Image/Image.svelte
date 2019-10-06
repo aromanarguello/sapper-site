49b51b393a74e820d13ccc174525c3c177238df8
Alejandro <aromanarguello@gmail.com>
2019-10-06 01:10:41 -0400

    finished header

---
+++ src/components/Image/Image.svelte
@@ -0,0 +1,18 @@
+<script>
+  export let colors;
+  const { navy } = colors;
+</script>
+
+<style>
+  img {
+    margin: 24px 0 0 0;
+    width: 250px;
+    height: 300px;
+    border: 1px solid var(--theme-color);
+  }
+</style>
+
+<img
+  src="https://res.cloudinary.com/dzeqyvxo2/image/upload/c_scale,h_1124,q_42/v1562175024/headshot_mfr0uy.jpg"
+  alt="profile-img"
+  style="--theme-color: {navy}" />
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,46 +1,73 @@
+<script>
+  import Image from "../components/Image/Image.svelte";
+
+  let colors = {
+    light: "#f9f7f7",
+    gray: "#dbe2ef",
+    blue: "#3f72af",
+    navy: "#112d4e"
+  };
+</script>
+
 <style>
-	h1, figure, p {
-		text-align: center;
-		margin: 0 auto;
-	}
+  :root {
+    --font-color: "#112d4e";
+  }
+  h1,
+  figure {
+    text-align: center;
+    margin: 0 auto;
+  }
 
-	h1 {
-		font-size: 2.8em;
-		text-transform: uppercase;
-		font-weight: 700;
-		margin: 0 0 0.5em 0;
-	}
+  h1 {
+    font-size: 2.5em;
+    font-weight: 600;
+    color: #112d4e;
+    margin: 24px 0 0 0;
+  }
 
-	figure {
-		margin: 0 0 1em 0;
-	}
+  @media (min-width: 480px) {
+    /* h1 {
+      font-size: 4em;
+    } */
+  }
 
-	img {
-		width: 100%;
-		max-width: 400px;
-		margin: 0 0 1em 0;
-	}
+  #header {
+    text-align: center;
+    display: flex;
+    color: #112d4e;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+  }
 
-	p {
-		margin: 1em auto;
-	}
+  .separator {
+    width: 100%;
+    height: 25px;
+    padding: 24px 0 24px 0;
+    margin: 0;
+    text-align: center;
+    font-size: 28px;
+    letter-spacing: 10px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
 
-	@media (min-width: 480px) {
-		h1 {
-			font-size: 4em;
-		}
-	}
+  #bio {
+    height: 300px;
+    border: 1px solid rebeccapurple;
+  }
 </style>
 
 <svelte:head>
-	<title>Sapper project template</title>
+  <title>Alejandro Roman - Software Engineer</title>
 </svelte:head>
-
-<h1>Great success!</h1>
-
-<figure>
-	<img alt='Borat' src='great-success.png'>
-	<figcaption>HIGH FIVE!</figcaption>
-</figure>
-
-<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
+<section id="header" aria-label="Alejandro Roman">
+  <figure>
+    <Image bind:colors />
+  </figure>
+  <h1 style="--theme-color: {colors.navy}">Alejandro Roman</h1>
+  <p class="separator" aria-hidden="true">{`.   .   .`}</p>
+</section>
+<section id="bio">Yo</section>
